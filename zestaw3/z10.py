4ac5ac4025ff53b2060b60499e74f446f2c49c8c
Tetiana <45294563+Tetiana2370@users.noreply.github.com>
2019-10-22 21:14:21 +0200

    Update z10.py

--- zestaw3/z10.py
+++ zestaw3/z10.py
@@ -1,17 +1,37 @@
-#   Zakładam że nie zdarzy się sytuacji typu: IIIIV, VVX
 DICTIONARY = {"I": 1, "V" : 5, "X":10, "C": 100, "D": 500, "M": 1000}
 
+def checkRoman(number):
+    S = dict()
+    prev = ""
+    for i in list(number):
+        if(i in S):
+            if(i != prev or S[i] >= 3):
+                return False
+            S[i]+=1
+        else:
+            S.update({i: 1})
+        prev = i
+    return True
+    
 def roman2int(number):
+    count = 1
     sum = 0
-    prev = 1
+    prev = 0
+    if checkRoman(number) == False:
+        print("FormatError in roman number: ", end=" ")
+        return -1
     for i in list(number):
         try:
-            if(DICTIONARY[i] > prev):
-                sum = DICTIONARY[i] - sum;
-            else: 
-                sum+= DICTIONARY[i]
+            if(DICTIONARY[i] == prev):
+                count += 1
+            elif(DICTIONARY[i] > prev):
+                sum -= prev*count*2
+                count = 1
+            sum += DICTIONARY[i]
+            if(DICTIONARY[i] != prev):
+                count = 1
         except KeyError:
-            print("value error in roman number")
+            print("KeyError in roman number: ", end="")
             return -1
         prev = DICTIONARY[i]
     return sum
@@ -23,4 +43,8 @@ print(roman2int("IX"))
 print(roman2int("XI"))
 print(roman2int("HIII"))
 print(roman2int("CM"))
+print(roman2int("XXXIV"))
+print(roman2int("XXXXIIIV"))
+
+
 
